2f2712b0513010b1bbcd3e31e4f88e56204c66ee
Paweł Szaga <Pawel.Szaga@luxmed.pl>
2020-10-17 18:23:34 +0200

    building scripts

---
+++ docker_build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd ./blue
+docker build --tag openterprise/blue:latest .
+cd ../green
+docker build --tag openterprise/green:latest .
+cd ../orange
+docker build --tag openterprise/orange:latest .
+cd ../red
+docker build --tag openterprise/red:latest .
+cd ../yellow
+docker build --tag openterprise/yellow:latest .
---
+++ docker_push.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd ./blue
+docker push openterprise/blue:latest
+cd ../green
+docker push openterprise/green:latest
+cd ../orange
+docker push openterprise/orange:latest
+cd ../red
+docker push openterprise/red:latest
+cd ../yellow
+docker push openterprise/yellow:latest
+
