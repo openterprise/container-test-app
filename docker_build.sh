4da4990ecdc78f508d3b3b4655bf8b6955e9c90f
Paweł Szaga <Pawel.Szaga@luxmed.pl>
2020-10-17 19:20:41 +0200

    adding new tools

--- docker_build.sh
+++ docker_build.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 cd ./blue
-docker build --tag openterprise/blue:latest .
+docker build --tag openterprise/blue:latest . &
 cd ../green
-docker build --tag openterprise/green:latest .
+docker build --tag openterprise/green:latest . &
 cd ../orange
-docker build --tag openterprise/orange:latest .
+docker build --tag openterprise/orange:latest . &
 cd ../red
-docker build --tag openterprise/red:latest .
+docker build --tag openterprise/red:latest . &
 cd ../yellow
-docker build --tag openterprise/yellow:latest .
+docker build --tag openterprise/yellow:latest . &
+
--- docker_push.sh
+++ docker_push.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 cd ./blue
-docker push openterprise/blue:latest
+docker push openterprise/blue:latest &
 cd ../green
-docker push openterprise/green:latest
+docker push openterprise/green:latest &
 cd ../orange
-docker push openterprise/orange:latest
+docker push openterprise/orange:latest &
 cd ../red
-docker push openterprise/red:latest
+docker push openterprise/red:latest &
 cd ../yellow
-docker push openterprise/yellow:latest
+docker push openterprise/yellow:latest &
 
